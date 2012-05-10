cc007b00aca7585c4ca717976ebb5bbf1ee968f3
Hugo Duncan <hugo@hugoduncan.org>
2012-05-10 09:54:14 -0400

    Add codox to dev dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :warn-on-reflection true
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :profiles {:dev {:dependencies [[codox-md "0.1.0"]
+                                  [codox/codox.core "0.6.1"]]}}
+  :codox {:writer codox-md.writer/write-docs
+          :output-dir "doc/0.1"})
